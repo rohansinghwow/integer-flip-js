bfe16c87c190de962d0e09fe7f3e742e349debe9
rohansinghwow <samfrankie439@gmail.com>
2021-03-03 16:27:21 +0530

    making the code more effecient and simple by removing code

--- flip.js
+++ flip.js
@@ -1,6 +1,6 @@
 function flipInteger(userInput){
-    userInput = -(userInput)  //  For example : -1 x 5 = -5 and -1 x -4 = 4
-    return userInput;
+      
+    return -userInput; //  For example : -1 x 5 = -5 and -1 x -4 = 4
 }
 
-console.log("Flipped integer is " + flipInteger(-23));
+console.log("Flipped integer is " + flipInteger(-33));
